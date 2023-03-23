29c84dad1278b7826ea4b08174d680ccd8d38cde
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-23 13:27:40 +0100

    Run Prettier

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.PORT || 8080;
-    
+
     await app.listen(port);
-    
+
     Logger.log(`Application is running on: http://localhost:${port}/`);
 }
 
