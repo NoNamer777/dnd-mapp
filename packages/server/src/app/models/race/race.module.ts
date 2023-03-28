873d5862f8f61b13b9d61aaea02f211c98ea1afa
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-28 09:36:38 +0200

    Add RaceEntity

--- packages/server/src/app/models/race/race.module.ts
+++ packages/server/src/app/models/race/race.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RaceService } from './race.service';
 import { RaceRepositoryProvider } from '../../providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RaceEntity } from './race.entity';
 
 @Module({
     providers: [RaceService, RaceRepositoryProvider],
+    imports: [TypeOrmModule.forFeature([RaceEntity])],
 })
 export class RaceModule {}
