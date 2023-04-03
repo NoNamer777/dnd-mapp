b73ea038a3553a2d513f15da50f5d6e34457c4c3
Oscar Wellner <oscar.wellner@gmail.com>
2023-04-03 14:20:06 +0200

    Set server host and port
    Using the ConfigService and environment variables

--- packages/server/src/environments/environment.ts
+++ packages/server/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    production: true,
+    production: process.env.SERVER_PRODUCTION || true,
+    host: process.env.SERVER_HOST || 'localhost',
+    port: process.env.SERVER_PORT || 8080,
 };
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,14 +1,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 8080;
+    const configServer = app.get(ConfigService);
 
-    await app.listen(port);
+    const { host, port } = {
+        host: configServer.get('host'),
+        port: configServer.get('port'),
+    };
 
-    Logger.log(`Application is running on: http://localhost:${port}/`);
+    await app.listen(port, host);
+
+    Logger.log(`Application is running on: http://${host}:${port}/`);
 }
 
 bootstrap();
