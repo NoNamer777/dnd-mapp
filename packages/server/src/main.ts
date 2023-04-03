36edcc5790771144abed52042e0f03fd61b2cb36
Oscar Wellner <oscar.wellner@gmail.com>
2023-04-03 21:04:13 +0200

    Use environment to setup ssl

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,19 +2,31 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const configServer = app.get(ConfigService);
+    const nestAppConfig =
+        process.env.SERVER_SECURE !== 'true'
+            ? {
+                  httpsOptions: {
+                      cert: readFileSync(process.env.SERVER_SSL_CERT),
+                      key: readFileSync(process.env.SERVER_SSL_KEY),
+                  },
+              }
+            : {};
 
-    const { host, port } = {
+    const nestApp = await NestFactory.create(AppModule, nestAppConfig);
+    const configServer = nestApp.get(ConfigService);
+
+    const { host, port, secure } = {
         host: configServer.get('host'),
         port: configServer.get('port'),
+        secure: configServer.get('secure'),
     };
 
-    await app.listen(port, host);
+    await nestApp.listen(port, host);
 
-    Logger.log(`Application is running on: http://${host}:${port}/`);
+    Logger.log(`Application is running on: http${secure ? 's' : ''}://${host}:${port}/`);
 }
 
 bootstrap();
