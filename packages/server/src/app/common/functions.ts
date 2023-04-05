44ea83fbe7ad09fb8ca96748cbe35b8a5a8198ee
Oscar Wellner <oscar.wellner@gmail.com>
2023-04-05 14:14:39 +0200

    Simplify IP adresses construction

--- packages/server/src/app/common/functions.ts
+++ packages/server/src/app/common/functions.ts
@@ -18,15 +18,14 @@ function determineIpAddress(host: string): string[] {
 
     for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
-            if (net.family === familyV4Value) {
-                if (!results[name]) {
-                    results[name] = [];
-                }
-                results[name].push(net.address);
+            // Skip over non-IPv4 addresses
+            if (net.family !== 'IPv4') {
+                continue;
             }
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
         }
     }
     return Object.values(results).flat();
