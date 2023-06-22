3315e74ee15fb15b3255cbf57067c165eb6a6183
Oscar Wellner <oscar.wellner@gmail.com>
2023-06-22 16:00:53 +0200

    Update import path after entities folder rename

--- packages/server/testing/mock/db/repositories/race-repository.provider.ts
+++ packages/server/testing/mock/db/repositories/race-repository.provider.ts
@@ -1,6 +1,6 @@
 import { mockRaceDb } from 'packages/data/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { RaceEntity } from '../../../../src/app/models/race';
+import { RaceEntity } from '../../../../src/app/entities/race';
 
 export const RaceRepositoryProvider = {
     provide: getRepositoryToken(RaceEntity),
