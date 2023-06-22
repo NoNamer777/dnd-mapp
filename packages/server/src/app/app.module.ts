533a20540b429322a821454707f52ef37c52eca1
Oscar Wellner <oscar.wellner@gmail.com>
2023-06-22 16:58:35 +0200

    Add AbilityModule

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmConfigModule } from './config';
 import { NestConfigModule } from './config/nest-config.module';
 import { RaceModule } from './entities/race';
+import { AbilityModule } from './entities/ability';
 
 @Module({
-    imports: [NestConfigModule, TypeOrmConfigModule, RaceModule],
+    imports: [NestConfigModule, TypeOrmConfigModule, RaceModule, AbilityModule],
 })
 export class AppModule {}
