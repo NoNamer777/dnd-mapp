fe53080b8fba0c2e68714550b91055643f1d0cd8
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-08 11:39:43 +0200

    Add database migrations

---
+++ packages/server/typeorm/migrations/1690464387448-ability-entity.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class AbilityEntity1690464387448 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'ability',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                        isUnique: true,
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('ability');
+    }
+}
