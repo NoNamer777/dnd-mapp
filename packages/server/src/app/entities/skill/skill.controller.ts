624c7f0363135970aa1ea8b880cd0cff3d238e3b
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-15 13:49:14 +0200

    Add update end-point for Skills

--- packages/server/src/app/entities/skill/skill.controller.ts
+++ packages/server/src/app/entities/skill/skill.controller.ts
@@ -1,13 +1,17 @@
 import {
+    BadRequestException,
     Body,
     Controller,
     Get,
     Param,
     ParseIntPipe,
     Post,
+    Put,
+    Req,
 } from '@nestjs/common';
 import { SkillService } from './skill.service';
 import { CreateSkillData, Skill } from '@dnd-mapp/data';
+import { Request } from 'express';
 
 @Controller('/api/skill')
 export class SkillController {
@@ -26,5 +30,17 @@ export class SkillController {
     @Get('/:id')
     async getById(@Param('id', ParseIntPipe) id: number) {
         return await this.skillService.findById(id);
+    }
+
+    @Put(':/id')
+    async update(@Req() request: Request, @Param('id', ParseIntPipe) id: number, @Body() data: Skill): Promise<Skill> {
+        const requestPath = request.path;
+
+        if (id !== data.id) {
+            throw new BadRequestException(
+                `Could not update Skill on path: '${requestPath}' with data from Skill with ID: '${data.id}'.`
+            );
+        }
+        return await this.skillService.update(data);
     }
 }
