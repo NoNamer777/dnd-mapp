0c6ca67c8c0d61966e6d6e031ba4ed7a66103fa5
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-15 13:58:25 +0200

    Add end-point param types

--- packages/server/src/app/entities/ability/ability.controller.ts
+++ packages/server/src/app/entities/ability/ability.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Put,
+    Req,
+} from '@nestjs/common';
 import { Ability } from '@dnd-mapp/data';
 import { AbilityService } from './ability.service';
+import { Request } from 'express';
 
 @Controller('/api/ability')
 export class AbilityController {
@@ -12,22 +24,33 @@ export class AbilityController {
     }
 
     @Post()
-    async create(data: Omit<Ability, 'id'>): Promise<Ability> {
+    async create(@Body() data: Omit<Ability, 'id'>): Promise<Ability> {
         return await this.abilityService.create(data);
     }
 
     @Get('/:id')
-    async getById(id: number): Promise<Ability> {
+    async getById(@Param('id', ParseIntPipe) id: number): Promise<Ability> {
         return await this.abilityService.findById(id);
     }
 
     @Put('/:id')
-    async update(data: Ability): Promise<Ability> {
+    async update(
+        @Req() request: Request,
+        @Body() data: Ability,
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<Ability> {
+        const requestPath = request.path;
+
+        if (id !== data.id) {
+            throw new BadRequestException(
+                `Could not update Ability on path: '${requestPath}' with data from Ability with ID: '${data.id}'.`
+            );
+        }
         return await this.abilityService.update(data);
     }
 
     @Delete('/:id')
-    async deleteById(id: number): Promise<void> {
+    async deleteById(@Param('id', ParseIntPipe) id: number): Promise<void> {
         await this.abilityService.deleteById(id);
     }
 }
