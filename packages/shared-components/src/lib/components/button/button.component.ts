e05ce103321142f7185415920f83bc574b81ead7
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-17 11:43:56 +0200

    Use attribute to determine dmaButtonType

--- packages/shared-components/src/lib/components/button/button.component.ts
+++ packages/shared-components/src/lib/components/button/button.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 type DmaButtonType = 'elevated' | 'filled' | 'tonal' | 'outlined' | 'text';
+
+const buttonTypeAttributeName = 'dma-button-type';
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
@@ -9,6 +11,22 @@ type DmaButtonType = 'elevated' | 'filled' | 'tonal' | 'outlined' | 'text';
     styleUrls: ['./button.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent {
-    @Input('dma-button') dmaButtonType: DmaButtonType = 'text';
+export class ButtonComponent implements OnInit {
+    @Input('dma-button') set dmaButtonType(buttonType: DmaButtonType) {
+        this.buttonType = buttonType;
+
+        this.updateRenderedAttribute();
+    }
+
+    private buttonType: DmaButtonType = 'text';
+
+    constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+    ngOnInit() {
+        this.updateRenderedAttribute();
+    }
+
+    private updateRenderedAttribute() {
+        this.renderer.setAttribute(this.elementRef.nativeElement, buttonTypeAttributeName, this.buttonType);
+    }
 }
