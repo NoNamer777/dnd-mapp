f92d963770dfaadfd0447195155d92d0a5560c5a
NoNamer777 <oscar.wellner@gmail.com>
2023-08-25 13:13:26 +0200

    Add theme models and extract light theme

---
+++ packages/shared-components/src/lib/theming/models.ts
@@ -0,0 +1,13 @@
+import { dmaLightTheme } from './themes';
+
+const dmaTheme = ['light'];
+
+export type DmaTheme = (typeof dmaTheme)[number];
+
+export function isDmaTheme(value: string): value is DmaTheme {
+    return dmaTheme.includes(value);
+}
+
+export type DmaThemeMap = Record<string, string>;
+
+export const dmaThemes = new Map<DmaTheme, DmaThemeMap>([['light', dmaLightTheme]]);
