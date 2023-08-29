fafb3fa41c76438fe13588e9971fb8db2e3cff39
NoNamer777 <oscar.wellner@gmail.com>
2023-08-29 18:43:44 +0200

    Inherit fillColor

--- packages/shared-components/src/lib/components/icons/plus/dma-plus-icon.component.ts
+++ packages/shared-components/src/lib/components/icons/plus/dma-plus-icon.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: `dma-icon[dma-plus-icon]`,
-    templateUrl: './dma-plus-icon.component.svg',
+    templateUrl: './dma-plus-icon.component.html',
     styleUrls: ['../_dma-icon.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
