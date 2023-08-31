1570d5fad0889ff3e16439eed909a8db6fe315ca
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-31 21:05:37 +0200

    Add tooltip story

---
+++ packages/shared-components/src/lib/tooltip/story/dma-tooltip.stories.ts
@@ -0,0 +1,36 @@
+import { moduleMetadata, StoryObj } from '@storybook/angular';
+import { Component, Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DmaTooltipModule } from '../dma-tooltip.module';
+import { DmaButtonModule } from '../../button';
+
+@Component({
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'plain-dma-tooltip-story',
+    templateUrl: './dma-tooltip.stories.html',
+})
+class PlainDmaTooltipStoryComponent {
+    @Input() tooltipText: string;
+}
+
+@NgModule({
+    imports: [CommonModule, DmaTooltipModule, DmaButtonModule],
+    declarations: [PlainDmaTooltipStoryComponent],
+    exports: [PlainDmaTooltipStoryComponent],
+})
+class StoryModule {}
+
+export default {
+    title: 'Dma-tooltip',
+    decorators: [moduleMetadata({ imports: [StoryModule] })],
+};
+
+export const PlainTooltip: StoryObj<PlainDmaTooltipStoryComponent> = {
+    args: {
+        tooltipText: 'My Tooltip',
+    },
+    render: (args) => ({
+        props: args,
+        template: `<plain-dma-tooltip-story></plain-dma-tooltip-story>`,
+    }),
+};
