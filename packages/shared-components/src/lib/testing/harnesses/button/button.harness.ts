c78304a1484e9742b69c6dcc784a41ca8b155818
Oscar Wellner <oscar.wellner@gmail.com>
2023-08-31 19:35:32 +0200

    Adjust selector for ButtonHarness

--- packages/shared-components/src/lib/testing/harnesses/button/button.harness.ts
+++ packages/shared-components/src/lib/testing/harnesses/button/button.harness.ts
@@ -1,10 +1,10 @@
 import { ComponentHarness } from '@angular/cdk/testing';
 
 export class ButtonHarness extends ComponentHarness {
-    static hostSelector = 'button[dma-button-type]';
+    static hostSelector = 'button[dma-button]';
 
     async getButtonType() {
-        return await (await this.host()).getAttribute('dma-button-type');
+        return await (await this.host()).getAttribute('dma-button');
     }
 
     async click() {
