14894b9d361078a1c6ebfdba506d9644bdf17aa7
NoNamer777 <oscar.wellner@gmail.com>
2023-09-04 21:43:10 +0200

    Add styles to the document element instead of the element with the theme directive so that cdk overlays are also styled correctly

--- packages/shared-components/src/lib/theming/diretives/dma-theme/dma-theme.directive.ts
+++ packages/shared-components/src/lib/theming/diretives/dma-theme/dma-theme.directive.ts
@@ -1,6 +1,7 @@
-import { Directive, HostBinding, Input, OnInit } from '@angular/core';
+import { Directive, HostBinding, Inject, Input, OnInit } from '@angular/core';
 import { DmaThemeService } from '../../services/dma-theme/dma-theme.service';
 import { DmaTheme, isDmaTheme } from '../../models';
+import { DOCUMENT } from '@angular/common';
 
 @Directive({
     selector: '[dmaTheme]',
@@ -13,16 +14,15 @@ export class DmaThemeDirective implements OnInit {
         this._theme = theme;
     }
 
-    @HostBinding('attr.style')
-    styling: string;
-
     @HostBinding('attr.dmaTheme')
     private _theme: DmaTheme = 'light';
 
-    constructor(private dmaThemeService: DmaThemeService) {}
+    constructor(private dmaThemeService: DmaThemeService, @Inject(DOCUMENT) private doc: Document) {}
 
     ngOnInit() {
         this.dmaThemeService.theme = this._theme;
-        this.styling = this.dmaThemeService.initializeTheme();
+
+        console.log(this.doc);
+        this.dmaThemeService.initializeTheme(this.doc.documentElement);
     }
 }
