4abb45a2208430853f614a68f999e4cdea732ff2
Oscar Wellner <oscar.wellner@gmail.com>
2023-09-07 21:26:00 +0200

    Add star icon

--- packages/shared-components/src/lib/icons/dma-icons.module.ts
+++ packages/shared-components/src/lib/icons/dma-icons.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DmaPlusIconComponent } from './plus/dma-plus-icon.component';
+import { DmaStarIconComponent } from './star/dma-star-icon.component';
 
-const icons = [DmaPlusIconComponent];
+const icons = [DmaPlusIconComponent, DmaStarIconComponent];
 
 @NgModule({
     imports: [CommonModule],
---
+++ packages/shared-components/src/lib/icons/star/dma-star-icon.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-icon[dma-star-icon]',
+    templateUrl: './dma-star-icon.component.html',
+    styleUrls: ['../_dma-icon.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DmaStarIconComponent {}
