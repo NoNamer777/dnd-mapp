4a7c05b2b13f51cd3ae4ecbd9aea52e25413457f
NoNamer777 <oscar.wellner@gmail.com>
2023-09-13 16:21:29 +0200

    Add testing path alias for shared-components/testing

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -2,6 +2,5 @@ export * from './lib/button';
 export * from './lib/icon-button';
 export * from './lib/icons';
 export * from './lib/state';
-export * from './lib/testing';
 export * from './lib/theming';
 export * from './lib/tooltip';
