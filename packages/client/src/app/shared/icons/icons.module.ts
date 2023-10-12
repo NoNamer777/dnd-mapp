988c649b1427ba85e4b5a41ea7f260c7e0c02327
Oscar Wellner <oscar.wellner@gmail.com>
2023-10-12 19:26:34 +0200

    Add IconsModule and enable fixedWidth for icons

---
+++ packages/client/src/app/shared/icons/icons.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+@NgModule({
+    imports: [FontAwesomeModule, FontAwesomeTestingModule],
+    exports: [FontAwesomeModule, FontAwesomeTestingModule],
+})
+export class IconsModule {
+    constructor(library: FaIconLibrary, config: FaConfig) {
+        library.addIcons(...icons);
+
+        config.fixedWidth = true;
+    }
+}
+
+const icons: IconDefinition[] = [];
