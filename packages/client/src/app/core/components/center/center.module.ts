2f8e1127dd46bf9d1b1d80acff007652efd8f7ce
NoNamer777 <oscar.wellner@gmail.com>
2023-10-18 23:02:52 +0200

    Show routed page in the center component

--- packages/client/src/app/core/components/center/center.module.ts
+++ packages/client/src/app/core/components/center/center.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CenterComponent } from './center.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     declarations: [CenterComponent],
     exports: [CenterComponent],
 })
