4f680f55d7e8cda2a9e9ab89956945df21f00b58
NoNamer777 <oscar.wellner@gmail.com>
2023-10-18 22:19:51 +0200

    Add root path and redirect every unknown path back to the root path

--- packages/client/src/app/core/app.routes.ts
+++ packages/client/src/app/core/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '/',
+    },
+    {
+        path: '**',
+        redirectTo: '/',
+    },
+];
