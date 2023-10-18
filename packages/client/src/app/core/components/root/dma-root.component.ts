de8d4efd9be4a6f98d3d3f7659b88d7bb407f1db
NoNamer777 <oscar.wellner@gmail.com>
2023-10-18 14:55:36 +0200

    Set the DmaTheme on the DmaRootComponent

--- packages/client/src/app/core/components/root/dma-root.component.ts
+++ packages/client/src/app/core/components/root/dma-root.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DmaThemeDirective } from '../../theming';
 
 @Component({
     selector: 'dma-root',
     templateUrl: './dma-root.component.html',
     styleUrls: ['./dma-root.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
+    hostDirectives: [DmaThemeDirective],
 })
 export class DmaRootComponent {}
--- packages/client/src/app/core/components/root/dma-root.module.ts
+++ packages/client/src/app/core/components/root/dma-root.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../../app.routes';
+import { DmaThemeModule } from '../../theming';
 import { CenterModule } from '../center';
 import { HeaderModule } from '../header';
 import { DmaRootComponent } from './dma-root.component';
@@ -12,6 +13,7 @@ import { DmaRootComponent } from './dma-root.component';
         RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
         HeaderModule,
         CenterModule,
+        DmaThemeModule,
     ],
     declarations: [DmaRootComponent],
     exports: [DmaRootComponent],
