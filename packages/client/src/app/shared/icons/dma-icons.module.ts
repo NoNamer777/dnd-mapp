29087aa4a4cd5853b8f1c323eb9fdcbd0ec32394
NoNamer777 <oscar.wellner@gmail.com>
2023-10-20 18:07:16 +0200

    Add icon for loading

--- packages/client/src/app/shared/icons/dma-icons.module.ts
+++ packages/client/src/app/shared/icons/dma-icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircleNotch, faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
     imports: [FontAwesomeModule],
@@ -15,4 +15,4 @@ export class DmaIconsModule {
     }
 }
 
-const icons: IconDefinition[] = [faCircleUser];
+const icons: IconDefinition[] = [faCircleUser, faCircleNotch];
