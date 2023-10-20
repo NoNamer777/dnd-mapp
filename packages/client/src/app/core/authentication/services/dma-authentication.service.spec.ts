f27b0f6daa45b8d9bc88963ae81844ee6a7615ea
NoNamer777 <oscar.wellner@gmail.com>
2023-10-20 18:00:27 +0200

    Add service for making authentication requests

---
+++ packages/client/src/app/core/authentication/services/dma-authentication.service.spec.ts
@@ -0,0 +1,53 @@
+import { HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { firstValueFrom } from 'rxjs';
+import { DmaHttpRequestTestingModule } from '../../../../testing';
+import { DmaAuthenticationService } from './dma-authentication.service';
+
+describe('DmaAuthenticationService', () => {
+    function setupTestEnvironment() {
+        TestBed.configureTestingModule({
+            imports: [DmaHttpRequestTestingModule],
+        });
+
+        return {
+            authenticationService: TestBed.inject(DmaAuthenticationService),
+            testController: TestBed.inject(HttpTestingController),
+        };
+    }
+
+    afterEach(() => {
+        TestBed.inject(HttpTestingController).verify();
+    });
+
+    it('should be processing log in requests without throwing errors.', async () => {
+        const { authenticationService, testController } = setupTestEnvironment();
+
+        const response = firstValueFrom(authenticationService.login('user1', 'secure_password'));
+        const request = testController.expectOne('http://localhost:8080/authentication/login');
+
+        request.flush(null, {
+            status: 200,
+            statusText: 'success',
+            headers: {
+                Authorization: 'Bearer token',
+            },
+        });
+
+        expect(async () => await response).not.toThrow();
+    });
+
+    it('should throw errors when processing httpErrorResponses', async () => {
+        const { authenticationService, testController } = setupTestEnvironment();
+
+        const response = firstValueFrom(authenticationService.login('user1', 'secure_password'));
+        const request = testController.expectOne('http://localhost:8080/authentication/login');
+
+        request.flush(null, {
+            status: 500,
+            statusText: 'internal server error',
+        });
+
+        await expectAsync(response).toBeRejected();
+    });
+});
