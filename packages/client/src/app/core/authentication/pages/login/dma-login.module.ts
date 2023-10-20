d1597b5f66b98b283d75a1667e1e3e94c8d5154a
NoNamer777 <oscar.wellner@gmail.com>
2023-10-20 18:08:22 +0200

    Handle login requests

--- packages/client/src/app/core/authentication/pages/login/dma-login.module.ts
+++ packages/client/src/app/core/authentication/pages/login/dma-login.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { DmaIconsModule } from '../../../../shared';
 import { DmaLoginPage } from './dma-login.page';
 
 @NgModule({
-    imports: [CommonModule, RouterModule, ReactiveFormsModule],
+    imports: [CommonModule, DmaIconsModule, ReactiveFormsModule, RouterModule],
     declarations: [DmaLoginPage],
     exports: [DmaLoginPage],
 })
--- packages/client/src/app/core/authentication/pages/login/dma-login.page.ts
+++ packages/client/src/app/core/authentication/pages/login/dma-login.page.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Subject, finalize, take, takeUntil } from 'rxjs';
+import { DmaAuthenticationService } from '../../services';
+
+const UNAUTHORIZED_ERROR_STATUS_CODE = 401;
 
 @Component({
     selector: 'dma-login',
@@ -7,17 +12,65 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     styleUrls: ['./dma-login.page.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DmaLoginPage {
+export class DmaLoginPage implements OnDestroy {
     form = new FormGroup({
         username: new FormControl<string | null>(null, [Validators.required]),
         password: new FormControl<string | null>(null, [Validators.required]),
     });
 
+    loading$ = new BehaviorSubject(false);
+    error$ = new BehaviorSubject<string | null>(null);
+    success$ = new BehaviorSubject<string | null>(null);
+
+    private destroy$ = new Subject<void>();
+
+    constructor(private authenticationService: DmaAuthenticationService) {}
+
     get isFormInvalid() {
         return this.form.invalid;
     }
 
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
     onSubmit() {
-        console.warn('Form submitted', this.form.value);
+        const { username, password } = this.form.value;
+
+        this.loading$.next(true);
+        this.error$.next(null);
+
+        this.authenticationService
+            .login(username!, password!)
+            .pipe(
+                take(1),
+                finalize(() => this.loading$.next(false)),
+                takeUntil(this.destroy$)
+            )
+            .subscribe({
+                next: () => this.onLoginSuccess(),
+                error: (error: HttpErrorResponse) => this.onLoginError(error),
+            });
+    }
+
+    private onLoginError(error: HttpErrorResponse) {
+        const message =
+            error.status === UNAUTHORIZED_ERROR_STATUS_CODE
+                ? 'Invalid username/password'
+                : 'Something unexpected went wrong while trying to log in. Try again later';
+
+        const formError =
+            error.status == UNAUTHORIZED_ERROR_STATUS_CODE ? { invalidCredentials: true } : { unexpectedError: true };
+
+        this.error$.next(message);
+        this.form.setErrors({
+            ...this.form.errors,
+            ...formError,
+        });
+    }
+
+    private onLoginSuccess() {
+        this.success$.next('Login successful');
     }
 }
