e71c2f43acc4ebff545260d396411abd47c45564
NoNamer777 <oscar.wellner@gmail.com>
2023-10-20 12:12:02 +0200

    Enable animations

--- packages/client/src/app/core/app.module.ts
+++ packages/client/src/app/core/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DmaRootComponent, DmaRootModule } from './components';
 
 @NgModule({
-    imports: [BrowserModule, DmaRootModule],
+    imports: [BrowserModule, BrowserAnimationsModule, DmaRootModule],
     bootstrap: [DmaRootComponent],
 })
 export class AppModule {}
