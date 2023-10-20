2b96ac9d0773a35b8633f6df19328d1ea3f91695
NoNamer777 <oscar.wellner@gmail.com>
2023-10-20 13:00:52 +0200

    Add DmaHttpRequestService to make requests to the back-end server

---
+++ packages/client/src/app/shared/http-request/dma-http-request.module.ts
@@ -0,0 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [HttpClientModule],
+    exports: [HttpClientModule],
+})
+export class DmaHttpRequestModule {}
---
+++ packages/client/src/app/shared/http-request/dma-http-request.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class DmaHttpRequestService {
+    private readonly baseURL = 'http://localhost:8080';
+
+    constructor(private httpClient: HttpClient) {}
+
+    post<T>(endPoint: string, data: T) {
+        return this.httpClient.post(this.baseURL + endPoint, data);
+    }
+
+    get(endPoint: string) {
+        return this.httpClient.get(this.baseURL + endPoint);
+    }
+
+    delete(endPoint: string) {
+        return this.httpClient.delete(this.baseURL + endPoint);
+    }
+
+    put<T>(endPoint: string, data: T) {
+        return this.httpClient.put(this.baseURL + endPoint, data);
+    }
+}
