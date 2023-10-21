b472dddff35c6251e88066240546c8c017f77514
NoNamer777 <oscar.wellner@gmail.com>
2023-10-21 18:01:10 +0200

    Resolve database properties from the environment variables

--- packages/server/typeorm/config.dev.ts
+++ packages/server/typeorm/config.dev.ts
@@ -1,4 +1,8 @@
 import { DataSource } from 'typeorm';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+
+type DataSourceTypeValue = MysqlConnectionOptions['type'] | SqliteConnectionOptions['type'];
 
 function getMigrationsPath() {
     const databaseFilesPath = process.env.DATABASE_FILES_PATH;
@@ -10,13 +14,13 @@ function getMigrationsPath() {
 
 // Defaults to a Sqlite database which will persist its data in memory.
 const config = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    database: 'dnd_mapp',
-    username: 'dnd_mapp_user',
-    password: 'Osc_Wel2626!',
+    type: (process.env.DATABASE_TYPE as DataSourceTypeValue) || 'sqlite',
+    database: process.env.DATABASE_LOCATION || './dnd_mapp_db.db',
     migrations: getMigrationsPath(),
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
 });
 
 export default config;
