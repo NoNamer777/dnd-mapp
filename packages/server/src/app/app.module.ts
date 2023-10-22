11f149e776dc36f5207c81879029a0107f450e6c
NoNamer777 <oscar.wellner@gmail.com>
2023-10-22 15:26:44 +0200

    Serve front-end client from a sub path of the back-end application

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmConfigModule } from './config';
+import { ServeClientAppModule, TypeOrmConfigModule } from './config';
 import { NestConfigModule } from './config/nest-config.module';
 import { AbilityModule } from './entities/ability';
 import { RaceModule } from './entities/race';
 import { SkillModule } from './entities/skill';
 
 @Module({
-    imports: [NestConfigModule, TypeOrmConfigModule, RaceModule, AbilityModule, SkillModule],
+    imports: [NestConfigModule, TypeOrmConfigModule, ServeClientAppModule, RaceModule, AbilityModule, SkillModule],
 })
 export class AppModule {}
