45ecf83b7115923457490b6a221dac5cc7fb7bba
NoNamer777 <oscar.wellner@gmail.com>
2023-10-25 19:26:16 +0200

    Activate User CRUD routing

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -5,9 +5,18 @@ import { NestConfigModule } from './config/nest-config.module';
 import { AbilityModule } from './entities/ability';
 import { RaceModule } from './entities/race';
 import { SkillModule } from './entities/skill';
+import { UserModule } from './entities/user';
 
 @Module({
-    imports: [NestConfigModule, TypeOrmConfigModule, ServeClientAppModule, RaceModule, AbilityModule, SkillModule],
+    imports: [
+        NestConfigModule,
+        TypeOrmConfigModule,
+        ServeClientAppModule,
+        RaceModule,
+        AbilityModule,
+        SkillModule,
+        UserModule,
+    ],
     controllers: [AppController],
 })
 export class AppModule {}
