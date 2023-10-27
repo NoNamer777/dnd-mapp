1fff397cdd36321230b5b10d5bed991bf7e9e987
NoNamer777 <oscar.wellner@gmail.com>
2023-10-27 21:53:08 +0200

    Create UserRole Table upon running migration for User Role

--- packages/server/typeorm/migrations/1698418398919-user-role.ts
+++ packages/server/typeorm/migrations/1698418398919-user-role.ts
@@ -26,9 +26,44 @@ export class UserRole1698418398919 implements MigrationInterface {
                 ],
             })
         );
+
+        await queryRunner.createTable(
+            new Table({
+                name: 'user_role',
+                columns: [
+                    {
+                        name: 'user_id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isNullable: false,
+                        isGenerated: false,
+                    },
+                    {
+                        name: 'role_id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isNullable: false,
+                        isGenerated: false,
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ['user_id'],
+                        referencedTableName: 'user',
+                        referencedColumnNames: ['id'],
+                    },
+                    {
+                        columnNames: ['role_id'],
+                        referencedTableName: 'role',
+                        referencedColumnNames: ['id'],
+                    },
+                ],
+            })
+        );
     }
 
     async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('user_role', true, true, true);
         await queryRunner.dropTable('role', true, true, true);
     }
 }
