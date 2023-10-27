b029033cfff41ad1252f144e671633ca0293a759
NoNamer777 <oscar.wellner@gmail.com>
2023-10-27 21:37:27 +0200

    Add mock user role provider for testing

--- packages/server/testing/mock/db/index.ts
+++ packages/server/testing/mock/db/index.ts
@@ -2,3 +2,4 @@ export * from './repositories/mock-ability-repository.provider';
 export * from './repositories/mock-race-repository.provider';
 export * from './repositories/mock-skill-repository.provider';
 export * from './repositories/mock-user-repository.provider';
+export * from './repositories/mock-user-role-repository.provider';
