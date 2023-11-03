83acf2d3a7421cf57a5e86b8c2dacff7620056b6
NoNamer777 <oscar.wellner@gmail.com>
2023-11-03 17:14:06 +0100

    Include migrations into the build output

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -1,4 +1,22 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const { basename } = require('path');
+const glob = require('glob');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => config);
+module.exports = composePlugins(withNx(), (config) => {
+    config.entry = {
+        ...config.entry,
+        ...glob.sync(`packages/server/db/migrations/*.ts`).reduce((entries, filename) => {
+            const migrationName = basename(filename, '.ts');
+
+            return Object.assign({}, entries, { [migrationName]: filename });
+        }, {}),
+    };
+
+    config.output = {
+        ...config.output,
+        libraryTarget: 'umd',
+        filename: (pathData) => (pathData.chunk.name === 'main' ? '[name].js' : 'db/migrations/[name].js'),
+    };
+    return config;
+});
