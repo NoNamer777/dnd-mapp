43912f9f19e2f2e832cbb5ebb343f03cb64d4cf1
NoNamer777 <oscar.wellner@gmail.com>
2023-11-08 11:53:40 +0100

    Add form control for passwords

--- packages/client/src/app/shared/icons/dma-icons.module.ts
+++ packages/client/src/app/shared/icons/dma-icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faCircleNotch, faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircleNotch, faCircleUser, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
     imports: [FontAwesomeModule],
@@ -15,4 +15,4 @@ export class DmaIconsModule {
     }
 }
 
-const icons: IconDefinition[] = [faCircleUser, faCircleNotch];
+const icons: IconDefinition[] = [faCircleUser, faCircleNotch, faEye, faEyeSlash];
--- packages/client/src/app/shared/index.ts
+++ packages/client/src/app/shared/index.ts
@@ -1,3 +1,4 @@
+export * from './form';
 export * from './http';
 export * from './icons';
 export * from './storage';
