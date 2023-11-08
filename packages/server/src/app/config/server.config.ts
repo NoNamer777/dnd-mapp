340aa14e04732bd725674846d1c4e2a5132c46eb
NoNamer777 <oscar.wellner@gmail.com>
2023-11-08 15:02:00 +0100

    Change the default fallback port where the back-end runs on when no other port is provided

--- packages/server/src/app/config/server.config.ts
+++ packages/server/src/app/config/server.config.ts
@@ -29,7 +29,7 @@ export interface ServerConfig {
 export default registerAs('server', () => ({
     production: Boolean(process.env.PRODUCTION) || false,
     host: process.env.HOST || 'localhost.dndmapp.net',
-    port: Number(process.env.PORT) || 443,
+    port: Number(process.env.PORT) || 80,
     useSsl: Boolean(process.env.USE_SSL) || Boolean(getSslConfig().ssl),
     address: process.env.ADDRESS,
     jwtSecret: process.env.JWT_SECRET,
