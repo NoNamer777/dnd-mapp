9cad38b9e38d11a2e50a69f842b6ab245259e101
NoNamer777 <oscar.wellner@gmail.com>
2023-12-05 16:20:09 +0100

    Temporarily remove the tests for the nonce interceptor since there are no tests

--- packages/client/src/app/shared/http/interceptors/nonce.interceptor.spec.ts
+++ packages/client/src/app/shared/http/interceptors/nonce.interceptor.spec.ts
@@ -1,18 +1,13 @@
-import { HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-import { DmaHttpRequestTestingModule } from '../../../../testing';
-import { DmaHttpRequestService } from '../dma-http-request.service';
-
-describe('NonceInterceptor', () => {
-    function setupTestEnvironment() {
-        TestBed.configureTestingModule({
-            imports: [DmaHttpRequestTestingModule],
-            providers: [DmaHttpRequestService],
-        });
-
-        return {
-            requestService: TestBed.inject(DmaHttpRequestService),
-            testController: TestBed.inject(HttpTestingController),
-        };
-    }
-});
+// describe('NonceInterceptor', () => {
+// function setupTestEnvironment() {
+//     TestBed.configureTestingModule({
+//         imports: [DmaHttpRequestTestingModule],
+//         providers: [DmaHttpRequestService],
+//     });
+//
+//     return {
+//         requestService: TestBed.inject(DmaHttpRequestService),
+//         testController: TestBed.inject(HttpTestingController),
+//     };
+// }
+// });
