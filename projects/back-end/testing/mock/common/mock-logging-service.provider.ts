4cec3eaf25d62048820a18673aba9724b02b28cd
NoNamer777 <oscar.wellner@gmail.com>
2024-01-19 18:59:56 +0100

    Mock loggin warning messages

--- projects/back-end/testing/mock/common/mock-logging-service.provider.ts
+++ projects/back-end/testing/mock/common/mock-logging-service.provider.ts
@@ -3,7 +3,15 @@ import { LoggerService } from '../../../src/app/common';
 class MockLoggingService extends LoggerService {
     logs: { [context: string]: string[] } = {};
 
+    override warn(message: string) {
+        this.addMessage(message);
+    }
+
     override log(message: string) {
+        this.addMessage(message);
+    }
+
+    private addMessage(message: string) {
         if (!this.logs[this.context]) {
             this.logs[this.context] = [];
         }
