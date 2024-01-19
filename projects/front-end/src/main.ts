4a604c5897d04bf37f3d559eaccc33854fc415f6
NoNamer777 <oscar.wellner@gmail.com>
2024-01-19 17:47:31 +0100

    Move script imports to be packaged by the task executor

--- projects/front-end/src/main.ts
+++ projects/front-end/src/main.ts
@@ -1,6 +1,3 @@
-import '@popperjs/core/dist/esm/popper.js';
-import 'bootstrap/dist/js/bootstrap.esm.min.js';
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig, DmaRootComponent } from './app/core';
 
