6608c325871fa16650e533d9557d50e88b0ab89c
NoNamer777 <oscar.wellner@gmail.com>
2024-01-25 17:37:45 +0100

    Fix UserModel test

--- projects/data/src/lib/authentication/user.model.spec.ts
+++ projects/data/src/lib/authentication/user.model.spec.ts
@@ -1,10 +1,9 @@
-import { Roles, UserModel } from '@dnd-mapp/data';
-import { defaultUser } from '../../../testing';
+import { Roles, UserBuilder } from '@dnd-mapp/data';
+import { defaultRole } from '@dnd-mapp/data/testing';
 
 describe('UserModel', () => {
     it('should verify that a User has a role', () => {
-        const { id, username, emailAddress, password } = defaultUser;
-        const user = new UserModel(username, password, emailAddress, id, [{ id: 1, name: Roles.PLAYER }]);
+        const user = new UserBuilder().withRoles([defaultRole]).build();
 
         expect(user.hasRole(Roles.DUNGEON_MASTER)).toEqual(false);
         expect(user.hasRole(Roles.ADMIN)).toEqual(false);
