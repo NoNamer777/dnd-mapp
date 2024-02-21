9262f9b44b5dd3ebaba674f54859e328d4b67bc9
NoNamer777 <oscar.wellner@gmail.com>
2024-02-21 15:03:54 +0100

    Add input label seperate input field, autocomplete, inputTypes and autofocus attributes

--- projects/shared-components/src/lib/input/dma-input.component.ts
+++ projects/shared-components/src/lib/input/dma-input.component.ts
@@ -1,13 +1,87 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+
+type DmaInputType = 'text' | 'password' | 'email' | 'search' | 'tel';
+
+type AutoComplete =
+    | 'off'
+    | 'on'
+    | 'name'
+    | 'honorific-prefix'
+    | 'given-name'
+    | 'additional-name'
+    | 'family-name'
+    | 'honorific-suffix'
+    | 'nickname'
+    | 'email'
+    | 'username'
+    | 'new-password'
+    | 'current-password'
+    | 'one-time-code'
+    | 'organization-title'
+    | 'organization'
+    | 'street-address'
+    | 'shipping'
+    | 'billing'
+    | 'address-line1'
+    | 'address-line2'
+    | 'address-line3'
+    | 'address-level4'
+    | 'address-level3'
+    | 'address-level2'
+    | 'address-level1'
+    | 'country'
+    | 'country-name'
+    | 'postal-code'
+    | 'cc-name'
+    | 'cc-given-name'
+    | 'cc-family-name'
+    | 'cc-number'
+    | 'cc-exp'
+    | 'cc-exp-month'
+    | 'cc-exp-year'
+    | 'cc-csc'
+    | 'cc-type'
+    | 'transaction-currency'
+    | 'transaction-amount'
+    | 'language'
+    | 'bday'
+    | 'bday-day'
+    | 'bday-month'
+    | 'bday-year'
+    | 'sex'
+    | 'tel'
+    | 'tel-country-code'
+    | 'tel-national'
+    | 'tel-area-code'
+    | 'tel-local'
+    | 'tel-extension'
+    | 'impp'
+    | 'url'
+    | 'photo'
+    | 'webauthn';
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
-    selector: 'input[dma-input]',
-    template: '',
+    selector: 'dma-input',
+    templateUrl: './dma-input.component.html',
     styleUrl: './dma-input.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
     imports: [CommonModule],
 })
-export class DmaInputComponent {}
+export class DmaInputComponent {
+    @Input() label: string;
+
+    @Input() @HostBinding('class.disabled') disabled = false;
+
+    @Input() @HostBinding('class.readonly') readonly = false;
+
+    @Input() value: string;
+
+    @Input() inputType: DmaInputType = 'text';
+
+    @Input() autocomplete: AutoComplete = 'off';
+
+    @Input() autofocus = false;
+}
