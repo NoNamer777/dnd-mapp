9a1095efebfb568326a9e31130603e7d5b47a952
NoNamer777 <oscar.wellner@gmail.com>
2024-02-28 16:09:19 +0100

    Extract input types and interfaces

--- projects/shared-components/src/lib/input/dma-input.component.ts
+++ projects/shared-components/src/lib/input/dma-input.component.ts
@@ -14,70 +14,9 @@ import {
     ViewChild,
 } from '@angular/core';
 import { DmaIconComponent } from '../icons';
+import { AnimationState, AutoComplete, DmaInputType, dmaInputValueAccessorProvider } from './dma-input.models';
 import { inputBorderAnimation } from './input-border.animation';
 import { inputLabelAnimation } from './input-label.animation';
-
-type DmaInputType = 'text' | 'password' | 'email' | 'search' | 'tel';
-
-type AutoComplete =
-    | 'off'
-    | 'on'
-    | 'name'
-    | 'honorific-prefix'
-    | 'given-name'
-    | 'additional-name'
-    | 'family-name'
-    | 'honorific-suffix'
-    | 'nickname'
-    | 'email'
-    | 'username'
-    | 'new-password'
-    | 'current-password'
-    | 'one-time-code'
-    | 'organization-title'
-    | 'organization'
-    | 'street-address'
-    | 'shipping'
-    | 'billing'
-    | 'address-line1'
-    | 'address-line2'
-    | 'address-line3'
-    | 'address-level4'
-    | 'address-level3'
-    | 'address-level2'
-    | 'address-level1'
-    | 'country'
-    | 'country-name'
-    | 'postal-code'
-    | 'cc-name'
-    | 'cc-given-name'
-    | 'cc-family-name'
-    | 'cc-number'
-    | 'cc-exp'
-    | 'cc-exp-month'
-    | 'cc-exp-year'
-    | 'cc-csc'
-    | 'cc-type'
-    | 'transaction-currency'
-    | 'transaction-amount'
-    | 'language'
-    | 'bday'
-    | 'bday-day'
-    | 'bday-month'
-    | 'bday-year'
-    | 'sex'
-    | 'tel'
-    | 'tel-country-code'
-    | 'tel-national'
-    | 'tel-area-code'
-    | 'tel-local'
-    | 'tel-extension'
-    | 'impp'
-    | 'url'
-    | 'photo'
-    | 'webauthn';
-
-type AnimationState = 'populated' | 'unpopulated';
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
