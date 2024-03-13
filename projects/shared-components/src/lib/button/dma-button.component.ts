af6f78291a42e7c87af2b317b7394b5e74e625c6
NoNamer777 <oscar.wellner@gmail.com>
2024-03-13 11:43:10 +0100

    Add DmaButtonTypes enum, attribute transform function, and simplify Input attribute

--- projects/shared-components/src/lib/button/dma-button.component.ts
+++ projects/shared-components/src/lib/button/dma-button.component.ts
@@ -2,15 +2,29 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
 import { DmaStateComponent, StateColors } from '../state';
 
-export type DmaButtonType = 'elevated' | 'filled' | 'tonal' | 'outlined' | 'text';
+export enum DmaButtonTypes {
+    ELEVATED = 'elevated',
+    FILLED = 'filled',
+    TONAL = 'tonal',
+    OUTLINED = 'outlined',
+    TEXT = 'text',
+}
+
+export type DmaButtonType = (typeof DmaButtonTypes)[keyof typeof DmaButtonTypes];
 
 const containerColorsPerButtonType = new Map<DmaButtonType, StateColors>([
-    ['elevated', { baseLayer: 'var(--surface-container-low)', stateLayer: 'var(--primary)' }],
-    ['filled', { baseLayer: 'var(--primary)', stateLayer: 'var(--on-primary)' }],
-    ['tonal', { baseLayer: 'var(--secondary-container)', stateLayer: 'var(--on-secondary-container)' }],
-    ['outlined', { baseLayer: 'var(--surface)', stateLayer: 'var(--primary)' }],
-    ['text', { baseLayer: 'transparent', stateLayer: 'var(--primary)' }],
+    [DmaButtonTypes.ELEVATED, { baseLayer: 'var(--surface-container-low)', stateLayer: 'var(--primary)' }],
+    [DmaButtonTypes.FILLED, { baseLayer: 'var(--primary)', stateLayer: 'var(--on-primary)' }],
+    [DmaButtonTypes.TONAL, { baseLayer: 'var(--secondary-container)', stateLayer: 'var(--on-secondary-container)' }],
+    [DmaButtonTypes.OUTLINED, { baseLayer: 'var(--surface)', stateLayer: 'var(--primary)' }],
+    [DmaButtonTypes.TEXT, { baseLayer: 'transparent', stateLayer: 'var(--primary)' }],
 ]);
+
+function dmaButtonTypeAttribute(buttonType: string | DmaButtonType) {
+    return Object.values(DmaButtonTypes as unknown as string[]).includes(buttonType)
+        ? (buttonType as DmaButtonType)
+        : DmaButtonTypes.TEXT;
+}
 
 @Component({
     // eslint-disable-next-line @angular-eslint/component-selector
@@ -22,17 +36,10 @@ const containerColorsPerButtonType = new Map<DmaButtonType, StateColors>([
     standalone: true,
     imports: [CommonModule, DmaStateDirective],
 })
-    @Input('dma-button') set dmaButtonType(buttonType: DmaButtonType | string) {
-        if (buttonType === '') return;
-
-        this.buttonType = buttonType as DmaButtonType;
-
-        this.updateRenderedAttribute();
-    }
-
 export class DmaButtonComponent extends DmaStateDirective {
     @HostBinding('attr.dma-button')
-    private buttonType: DmaButtonType = 'text';
+    @Input({ alias: 'dma-button', transform: dmaButtonTypeAttribute })
+    private buttonType: DmaButtonType;
 
     ngOnInit() {
         this.updateRenderedAttribute();
