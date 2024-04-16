4be875e66236a4c9e3198ef621c0a761bdfaf14c
NoNamer777 <oscar.wellner@gmail.com>
2024-04-16 23:25:07 +0200

    Add RouteGuard to prevent unauthenticated access

---
+++ projects/front-end/src/app/core/authentication/guards/authentication.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { map } from 'rxjs';
+import { DmaAuthenticationService } from '../services';
+
+export function authenticationGuard(skipWhenAuthenticated = false) {
+    return (_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> => {
+        const authenticationService = inject(DmaAuthenticationService);
+        const router = inject(Router);
+
+        return authenticationService.authenticatedUser$.pipe(
+            map(Boolean),
+            map((isAuthenticated) => {
+                if ((!skipWhenAuthenticated && isAuthenticated) || (!isAuthenticated && skipWhenAuthenticated)) {
+                    return true;
+                }
+                return !isAuthenticated
+                    ? router.parseUrl(`/login?redirectTo=${state.url}`)
+                    : router.parseUrl('/characters');
+            })
+        );
+    };
+}
