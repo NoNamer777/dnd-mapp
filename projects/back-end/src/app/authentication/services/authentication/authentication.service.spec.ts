c29b7983968e4f88095bf0a2e8873b5939664a77
NoNamer777 <oscar.wellner@gmail.com>
2024-04-25 16:19:17 +0200

    Remove service tests

--- projects/back-end/src/app/authentication/services/authentication/authentication.service.spec.ts
+++ projects/back-end/src/app/authentication/services/authentication/authentication.service.spec.ts
@@ -1,8 +1,3 @@
-import { UserBuilder } from '@dnd-mapp/data';
-import { defaultSession, defaultUser, mockSessionDB, mockTokenDB, mockUserDB } from '@dnd-mapp/data/testing';
-import { ConfigService } from '@nestjs/config';
-import { Test } from '@nestjs/testing';
-import crypto from 'crypto';
 import {
     mockLoggingServiceProvider,
     mockRoleModuleProviders,
@@ -10,12 +5,14 @@ import {
     mockTokenModuleProviders,
     mockUserModuleProviders,
 } from '@dnd-mapp/back-end/testing';
+import { ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
 import { buildServerUrl } from '../../../common';
 import { DndMappJwtModule, NestConfigModule, ServerConfig } from '../../../config';
 import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationService', () => {
-    async function setupTestEnvironment() {
+    async function setupTest() {
         const module = await Test.createTestingModule({
             imports: [NestConfigModule, DndMappJwtModule],
             providers: [
@@ -37,98 +34,8 @@ describe('AuthenticationService', () => {
         };
     }
 
-    it('should return the active Session tokens for a User', async () => {
-        defaultSession.codeChallenge = crypto.createHash('sha256').update('code_challenge').digest().toString('base64');
-
-        defaultSession.authorizationCode = 'authorization_code';
-        defaultSession.authCodeGeneratedAt = new Date();
-
-        const { service } = await setupTestEnvironment();
-
-        // Generate the tokens
-        await service.login({ username: defaultUser.username, password: 'secure_password' }, defaultSession);
-
-        const tokens = await service.getTokensForSession(
-            defaultSession,
-            defaultUser.username,
-            'code_challenge',
-            'authorization_code'
-        );
-
-        expect(tokens.size).toEqual(3);
-    });
-
-    it('should generate an authorization code for a Session', async () => {
-        const { service } = await setupTestEnvironment();
-
-        await expect(service.generateAuthorizationCode(defaultSession.id)).resolves.not.toThrow();
-    });
-
-    it('should handle sign up requests', async () => {
-        const { service } = await setupTestEnvironment();
-        const user = await service.signup(
-            new UserBuilder()
-                .withUsername('User2')
-                .withPassword('secure_password')
-                .withPassword('user2@domain.com')
-                .build()
-        );
-
-        expect(user.id).toEqual(expect.any(Number));
-        expect(mockUserDB.findAll()).toHaveLength(2);
-        expect(mockUserDB.findOneByUsername('User2')).toEqual(expect.objectContaining(user));
-    });
-
-    it('should store a code challenge for a Session', async () => {
-        const { service } = await setupTestEnvironment();
-        const { id } = defaultSession;
-
-        await service.storeCodeChallenge(id, 'code_challenge');
-
-        expect(mockSessionDB.findOneById(id).codeChallenge).toEqual('code_challenge');
-    });
-
-    it('should revoke active tokens on log out', async () => {
-        const { service } = await setupTestEnvironment();
-
-        await service.login({ username: defaultUser.username, password: 'secure_password' }, defaultSession);
-
-        expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(3);
-
-        await service.logout(defaultUser, defaultSession);
-
-        expect(mockTokenDB.findActiveTokensForUserSession(defaultUser.id, defaultSession.id)).toHaveLength(0);
-    });
-
-    describe('login', () => {
-        it('should handle login requests', async () => {
-            const { service } = await setupTestEnvironment();
-
-            await expect(
-                service.login({ username: defaultUser.username, password: 'secure_password' }, defaultSession)
-            ).resolves.not.toThrow();
-
-            expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(3);
-        });
-
-        it('should throw an 401 when providing an incorrect password', async () => {
-            const { service } = await setupTestEnvironment();
-
-            await expect(
-                service.login({ username: defaultUser.username, password: 'incorrect_password' }, defaultSession)
-            ).rejects.toThrow('Invalid username/password');
-
-            expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(0);
-        });
-
-        it('should throw an 401 when unable to find the User', async () => {
-            const { service } = await setupTestEnvironment();
-
-            await expect(
-                service.login({ username: 'Bob', password: defaultUser.password }, defaultSession)
-            ).rejects.toThrow('Invalid username/password');
-
-            expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(0);
-        });
+    it('should initialize', async () => {
+        const { service } = await setupTest();
+        expect(service).toBeDefined();
     });
 });
--- projects/back-end/src/app/authentication/services/token/token.service.spec.ts
+++ projects/back-end/src/app/authentication/services/token/token.service.spec.ts
@@ -1,4 +1,3 @@
-import { defaultSession, defaultUser, mockTokenDB } from '@dnd-mapp/data/testing';
 import { mockLoggingServiceProvider, mockTokenModuleProviders } from '@dnd-mapp/back-end/testing';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
@@ -8,7 +7,7 @@ import { DndMappJwtModule, NestConfigModule, ServerConfig } from '../../../confi
 import { TokenService } from './token.service';
 
 describe('TokenService', () => {
-    async function setupEnvironment() {
+    async function setupTest() {
         const module = await Test.createTestingModule({
             imports: [DndMappJwtModule, NestConfigModule],
             providers: [mockLoggingServiceProvider, ...mockTokenModuleProviders],
@@ -24,59 +23,8 @@ describe('TokenService', () => {
         };
     }
 
-    it('should generate tokens for a UserSession', async () => {
-        const { tokenService } = await setupEnvironment();
-
-        await expect(tokenService.generateTokensForUser(defaultUser, defaultSession)).resolves.not.toThrow();
-
-        expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(3);
-    });
-
-    it('should clean up tokens for a User after max tokens have been reached', async () => {
-        const { tokenService } = await setupEnvironment();
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-        expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(3);
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-        expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(6);
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-        expect(mockTokenDB.findAllTokensForUser(defaultUser.id)).toHaveLength(6);
-    });
-
-    it('should revoke active tokens when generating new tokens for a UserSession', async () => {
-        const { tokenService } = await setupEnvironment();
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-
-        const previouslyActiveTokens = mockTokenDB.findActiveTokensForUserSession(defaultUser.id, defaultSession.id);
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-
-        const currentlyActiveTokens = mockTokenDB.findActiveTokensForUserSession(defaultUser.id, defaultSession.id);
-
-        expect(currentlyActiveTokens).not.toContain(expect.arrayContaining(previouslyActiveTokens));
-    });
-
-    it('should return encoded token data', async () => {
-        const { tokenService, jwtService } = await setupEnvironment();
-
-        await tokenService.generateTokensForUser(defaultUser, defaultSession);
-
-        const tokenData = await tokenService.getEncodedTokensForUserSession(defaultUser, defaultSession);
-        const tokens = mockTokenDB.findActiveTokensForUserSession(defaultUser.id, defaultSession.id);
-
-        for (const data of tokenData) {
-            const token = tokens.find((token) => token.type === data[0]);
-            const decodedToken = jwtService.decode(data[1].token);
-
-            expect(token.jti).toEqual(decodedToken.jti);
-            expect(token.user.id).toEqual(decodedToken.sub);
-            expect(token.session.id).toEqual(decodedToken.ses);
-            expect(Math.floor(token.issuedAt.getTime() / 1_000)).toEqual(decodedToken.iat);
-            expect(Math.floor(token.notBefore.getTime() / 1_000)).toEqual(decodedToken.nbf);
-            expect(Math.floor(token.expiresAt.getTime() / 1_000)).toEqual(decodedToken.exp);
-        }
+    it('should initialize', async () => {
+        const { tokenService } = await setupTest();
+        expect(tokenService).toBeDefined();
     });
 });
