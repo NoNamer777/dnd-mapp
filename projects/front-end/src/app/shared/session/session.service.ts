9a56f5b02fea3382efd3068097e0ecb696fa8915
NoNamer777 <oscar.wellner@gmail.com>
2024-04-29 23:07:47 +0200

    Rename Session data model

--- projects/front-end/src/app/shared/session/session.service.ts
+++ projects/front-end/src/app/shared/session/session.service.ts
@@ -1,20 +1,20 @@
 import { DestroyRef, inject, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { SessionModel } from '@dnd-mapp/data';
+import { Session } from '@dnd-mapp/data';
 import { plainToInstance } from 'class-transformer';
 import { BehaviorSubject, tap } from 'rxjs';
 import { DmaHttpRequestService } from '../http';
 
 @Injectable({ providedIn: 'root' })
 export class SessionService {
-    readonly session$ = new BehaviorSubject<SessionModel>(null);
+    readonly session$ = new BehaviorSubject<Session>(null);
 
     private readonly requestService = inject(DmaHttpRequestService);
     private readonly destroyRef = inject(DestroyRef);
 
     retrieveSession() {
-        return this.requestService.post<SessionModel>('/session', null, { withState: true }).pipe(
-            tap((session) => this.session$.next(plainToInstance(SessionModel, session))),
+        return this.requestService.post<Session>('/session', null, { withState: true }).pipe(
+            tap((session) => this.session$.next(plainToInstance(Session, session))),
             takeUntilDestroyed(this.destroyRef)
         );
     }
