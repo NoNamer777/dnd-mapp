55179ab1398461995bb9d5d3240a05ed468ace57
NoNamer777 <oscar.wellner@gmail.com>
2024-05-14 07:36:24 +0200

    Fix test for UserService after rename HttpRequestService

--- projects/front-end/src/app/user/services/user.service.spec.ts
+++ projects/front-end/src/app/user/services/user.service.spec.ts
@@ -1,16 +1,18 @@
 import { provideHttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { defaultUser } from '@dnd-mapp/data/testing';
-import { withInitializedConfig } from '@dnd-mapp/front-end/testing';
+import { TEST_INITIALIZER, withInitializedConfig } from '@dnd-mapp/front-end/testing';
 import { createId } from '@paralleldrive/cuid2';
 import { firstValueFrom } from 'rxjs';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-    function setupTestEnvironment() {
+    async function setupTest() {
         TestBed.configureTestingModule({
             providers: [withInitializedConfig(), provideHttpClient(), UserService],
         });
+
+        await TestBed.inject(TEST_INITIALIZER)();
 
         return {
             service: TestBed.inject(UserService),
@@ -18,14 +20,14 @@ describe('UserService', () => {
     }
 
     it('should return a User by their ID', async () => {
-        const { service } = setupTestEnvironment();
+        const { service } = await setupTest();
         const { id, username } = defaultUser;
 
         expect(await firstValueFrom(service.getById(id))).toEqual(jasmine.objectContaining({ id, username }));
     });
 
     it('should throw a 404 if a User was not found by ID', async () => {
-        const { service } = setupTestEnvironment();
+        const { service } = await setupTest();
 
         await expectAsync(firstValueFrom(service.getById(createId()))).toBeRejected();
     });
--- projects/front-end/src/app/user/services/user.service.ts
+++ projects/front-end/src/app/user/services/user.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 import { UserModel } from '@dnd-mapp/data';
 import { plainToInstance } from 'class-transformer';
 import { map } from 'rxjs';
-import { DmaHttpRequestService } from '../../shared';
+import { HttpRequestService } from '../../shared';
 import { UserResponse } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(private readonly requestService: DmaHttpRequestService) {}
+    constructor(private readonly requestService: HttpRequestService) {}
 
     getById(userId: string) {
         return this.requestService
