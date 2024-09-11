1529f0b64ff5fe558834eeccfe98e0d3a2439e12
NoNamer777 <oscar.wellner@gmail.com>
2024-09-11 09:48:56 +0200

    Add basic layout styling

--- apps/authentication-server-ui/src/app/core/root/root.component.ts
+++ apps/authentication-server-ui/src/app/core/root/root.component.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
     selector: 'dma-root',
     templateUrl: './root.component.html',
+    styleUrl: './root.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
     imports: [RouterModule],
