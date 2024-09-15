421fb32ebdc7e1f472064f47dd2e8c850a27254c
NoNamer777 <oscar.wellner@gmail.com>
2024-09-15 16:07:37 +0200

    Setup basic routing to home page with route not-found page as fallback for unknown routes

--- apps/authentication-server-ui/src/app/core/config/app.routes.ts
+++ apps/authentication-server-ui/src/app/core/config/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        loadComponent: async () => (await import('../home/home.page')).HomePage,
+    },
+    {
+        path: 'not-found',
+        loadComponent: async () => (await import('../not-found/not-found.page')).NotFoundPage,
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found',
+    },
+];
