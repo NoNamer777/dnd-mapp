ee4ea9d749bb20213b80d63b793bd11256032758
NoNamer777 <oscar.wellner@gmail.com>
2024-09-19 11:57:46 +0200

    Use percentage of browser font size for root font size

---
+++ apps/authentication-server-ui/src/app/shared/components/side-bar/side-bar.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { SideBarHarness } from '@dnd-mapp/authentication-server-ui/testing';
+import { SideBarComponent } from './side-bar.component';
+
+describe('SideBarComponent', () => {
+    @Component({
+        template: `<dma-side-bar />`,
+    })
+    class TestComponent {}
+
+    async function setupTest() {
+        TestBed.configureTestingModule({
+            imports: [SideBarComponent],
+            declarations: [TestComponent],
+            providers: [provideExperimentalZonelessChangeDetection()],
+        });
+
+        const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(TestComponent));
+
+        return {
+            harness: await harnessLoader.getHarness(SideBarHarness),
+        };
+    }
+
+    it('should create', async () => {
+        const { harness } = await setupTest();
+        expect(harness).not.toBeNull();
+    });
+});
---
+++ apps/authentication-server-ui/src/app/shared/components/side-bar/side-bar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-side-bar',
+    templateUrl: './side-bar.component.html',
+    styleUrl: './side-bar.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+})
+export class SideBarComponent {}
