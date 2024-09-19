8a20838695b6158d223c56b00503313b46c0add4
NoNamer777 <oscar.wellner@gmail.com>
2024-09-19 11:16:26 +0200

    Import compiler to work with vitest browser mode

--- apps/authentication-server-ui/src/testing/setup-test.ts
+++ apps/authentication-server-ui/src/testing/setup-test.ts
@@ -1,3 +1,4 @@
+import '@angular/compiler';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
