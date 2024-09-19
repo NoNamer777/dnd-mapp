eb83f8eb3312f656a9cdc5dc57d203f7d829481f
NoNamer777 <oscar.wellner@gmail.com>
2024-09-19 16:43:28 +0200

    Add plus icon

---
+++ apps/authentication-server-ui/src/app/shared/components/icons/plus/plus.icon.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-plus-icon, dma-icon[dma-plus-icon]',
+    templateUrl: './plus.icon.svg',
+    styleUrl: '../icons.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+})
+export class PlusIcon {}
