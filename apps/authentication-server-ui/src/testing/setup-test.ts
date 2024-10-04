9b5ec23206db3792c5d8b5d009410d20df42cd59
NoNamer777 <oscar.wellner@gmail.com>
2024-10-04 10:24:48 +0200

    Use msw during testing and development

--- apps/authentication-server-ui/src/testing/setup-test.ts
+++ apps/authentication-server-ui/src/testing/setup-test.ts
@@ -1,5 +1,12 @@
 import '@angular/compiler';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { resetListeners, startServer, stopServer } from './mocks/authentication-server';
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+beforeAll(() => startServer());
+
+beforeEach(() => resetListeners());
+
+afterAll(() => stopServer());
