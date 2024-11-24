60ae8f0f19d2f36c07249895c936eeebd50873d7
NoNamer777 <oscar.wellner@gmail.com>
2024-11-24 17:43:02 +0100

    Enable animations

--- apps/authentication-server-ui/src/app/core/config/app.config.ts
+++ apps/authentication-server-ui/src/app/core/config/app.config.ts
@@ -1,9 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { provideTranslations } from '../../shared';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(appRoutes), provideHttpClient(), provideTranslations()],
+    providers: [provideRouter(appRoutes), provideHttpClient(), provideAnimationsAsync(), provideTranslations()],
 };
