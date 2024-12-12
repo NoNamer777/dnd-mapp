43ced32e3bbc54b8b7bc36dfccda1fe1d52bf3cf
NoNamer777 <oscar.wellner@gmail.com>
2024-12-12 11:33:20 +0100

    Add circle SVG

---
+++ apps/authentication-server-ui/src/app/shared/components/icons/circle/circle.icon.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'dma-circle-icon, dma-icon[dma-circle-icon]',
+    templateUrl: './circle.icon.svg',
+    styleUrl: '../icons.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+})
+export class CircleIcon {}
