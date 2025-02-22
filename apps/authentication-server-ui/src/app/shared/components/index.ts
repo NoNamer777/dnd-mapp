b784404a28d8622ffe12ff91f395e9d30a9ef0f0
NoNamer777 <oscar.wellner@gmail.com>
2025-02-22 17:32:53 +0100

    Add InputComponent

--- apps/authentication-server-ui/src/app/shared/components/index.ts
+++ apps/authentication-server-ui/src/app/shared/components/index.ts
@@ -1,6 +1,7 @@
 export * from './button';
 export * from './dialog';
 export * from './icons';
+export * from './input';
 export * from './navigation';
 export * from './side-bar';
 export * from './table';
